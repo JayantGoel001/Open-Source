52434277847d92f18a92264eb2d988a9be2e7ebc
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-03-25 11:28:39 +0530

    Initial Commit

--- Week2/6.py
+++ Week2/6.py
@@ -1,1 +1,15 @@
- 
+text = "hello world"
+textlines = ['Hello World\n', 'How is It Going?\n', "maybe\n"]
+filename = "helloworld.txt"
+
+with open(filename, 'w+') as f:
+    f.write(text)
+
+with open(filename, 'r+') as f:
+    print(f.read())
+
+with open(filename, 'w+') as f:
+    f.writelines(textlines)
+
+with open(filename, 'r+') as f:
+    print(f.readlines())
