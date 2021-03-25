103f9d579ed6d67832c412854bbd0ab2ebc464e9
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-03-25 11:38:31 +0530

    Initial Commit

--- Week2/8.py
+++ Week2/8.py
@@ -1,1 +1,6 @@
- 
+l = []
+with open("helloworld.txt",'r+') as f:
+    l = f.readlines()
+
+l = l[::-1]
+print(l)
--- Week2/9.py
+++ Week2/9.py
@@ -1,1 +1,6 @@
- 
+l = []
+with open("helloworld.txt",'r+') as f:
+    l = f.readlines()
+
+for i in l:
+    print(i[::-1],end="")
