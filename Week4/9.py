17f5f0465ceba0b07aa2792e71f1647dec770f1a
JayantGoel001 <jgoel92@gmail.com>
2021-04-22 01:22:07 +0530

    Initial Commit

---
+++ Week4/9.py
@@ -0,0 +1,6 @@
+from scipy import cluster, constants, fftpack, integrate, interpolate, io, linalg, ndimage, odr, optimize, signal, sparse, spatial, special
+import numpy as np
+
+ar = np.ones((4, 4))
+np.savetxt("test.text",ar)
+
